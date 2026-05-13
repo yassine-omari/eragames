2e226b62ec3d685fe5524e5617623abb5df4744d
yassine-omari <yo0546436@gmail.com>
2026-05-13 08:14:23 +0100

    fix log out bug

--- src/app/api/auth/signout/route.js
+++ src/app/api/auth/signout/route.js
@@ -2,5 +2,5 @@ import { cookies } from "next/headers";
 
 export async function GET() {
   (await cookies()).delete("session");
-  return Response.redirect("http://localhost:3000/login");
+  return Response.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/login`);
 }
